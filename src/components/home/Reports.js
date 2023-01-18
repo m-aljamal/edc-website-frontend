e643ddce3788f77f0bd0779d269a8b7ab3164ceb
Mohammad AL JAMAL <mohammadjamol@gmail.com>
2023-01-18 11:05:42 +0300

    reports 2

--- src/components/home/Reports.js
+++ src/components/home/Reports.js
@@ -26,7 +26,31 @@ const Reports = () => {
   return (
     <div className="container py-20">
       <Title title={reportTitle} />
-      
+      <div className="grid lg:grid-cols-2 grid-cols-2 gap-8 mt-12  ">
+        {reports.nodes.map(({ cover, id, pdf_url }) => (
+          <a
+            target="_blank"
+            rel="noopener noreferrer"
+            href={pdf_url}
+            key={id}
+            className="relative transition duration-150 ease-out hover:scale-110 lg:col-span-full  lg:w-1/3 lg:mx-auto "
+          >
+            <Img
+              fluid={cover.asset.fluid}
+              alt={reportTitle}
+              className="rounded-md shadow-2xl "
+            />
+
+            <div className=" absolute bg-gray-400 z-20 top-0 bottom-0 left-0 right-0 rounded-md opacity-0 hover:opacity-75 transition  ">
+              <div className=" flex justify-center items-center h-full ">
+                <div className=" bg-mainblue rounded-full w-28 h-28 flex justify-center items-center ">
+                  <FiDownload className="text-white  text-6xl " />
+                </div>
+              </div>
+            </div>
+          </a>
+        ))}
+      </div>
     </div>
   )
 }
